57a64983f1582993d465c208f8589df8e55fcffa
shivamsingha <shivamgit@pm.me>
2022-11-30 09:50:54 +0530

    Update nav test
    Signed-off-by: shivamsingha <shivamgit@pm.me>

--- client/src/components/Nav.test.js
+++ client/src/components/Nav.test.js
@@ -1,6 +1,7 @@
 import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import { renderWithProviders } from '../utils/test-utils';
 import Nav from './Nav';
@@ -12,6 +13,31 @@ test('nav rendering', async () => {
       <Nav />
     </Router>
   );
+
+  await screen.findByText(/sign in/i);
+  await screen.findByText(/sign up/i);
+});
+
+test('nav rendering signed in', async () => {
+  const history = createMemoryHistory();
+  renderWithProviders(
+    <Router history={history}>
+      <Nav />
+    </Router>,
+    {
+      preloadedState: {
+        userReducer: {
+          username: 'qwer',
+          email: 'a@gmail.com',
+          isLogged: true,
+        },
+      },
+    }
+  );
+
+  await screen.findByText(/new entry/i);
+  const signout = await screen.findByText(/sign out/i);
+  userEvent.click(signout);
 
   await screen.findByText(/sign in/i);
   await screen.findByText(/sign up/i);
